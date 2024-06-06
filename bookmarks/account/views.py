ce03e7673c77cd7aced85c04b6090105947af4d1
danaendraraj05 <danaendraraj@testpress.in>
2024-06-06 14:58:52 +0530

    Implemented Signals

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -12,6 +12,8 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
 
 class CustomLogoutView(LogoutView):
     template_name = 'registration/logged_out.html'
@@ -24,9 +26,20 @@ class CustomLogoutView(LogoutView):
 
 @login_required
 def dashboard(request):
+    
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                    flat=True)
+    if following_ids:
+ # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.select_related('user', 'user__profile')[:10]\
+                         .prefetch_related('target')[:10]
+    
     return render(request,
                 'account/dashboard.html',
-                        {'section': 'dashboard'})
+                        {'section': 'dashboard',
+                          'actions': actions})
 
 def user_login(request):
     if request.method == 'POST':
@@ -59,6 +72,7 @@ def register(request):
  # Save the User object
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,'account/register_done.html',{'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -118,6 +132,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
